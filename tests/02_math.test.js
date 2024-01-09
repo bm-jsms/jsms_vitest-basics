679a100c38c18a68adb2ca9c82fbfa5784cb8c6c
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-08 22:12:59 -0500

    test - class Math

---
+++ tests/02_math.test.js
@@ -0,0 +1,20 @@
+import { it } from 'vitest';
+import Math from '../src/02_math';
+
+describe('Math test', () => {
+	it('sum', () => {
+		expect(Math.sum(1, 2)).toBe(3);
+	});
+	it('sub', () => {
+		expect(Math.sub(5, 2)).toBe(3);
+	});
+	it('mul', () => {
+		expect(Math.mul(4, 2)).toBe(8);
+	});
+	it('div', () => {
+		expect(Math.div(6, 2)).toBe(3);
+	});
+	it('div by zero', () => {
+		expect(Math.div(6, 0)).toBe(null);
+	});
+});
